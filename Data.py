e5675e557ac4f3fb436cf15932137e04892873d6
Ahmet Sivri <Ahmet.Sivri@cleverspeck.com>
2020-12-06 03:27:34 -0500

    First commit, dictionary data load is added

---
+++ Data.py
@@ -0,0 +1,5 @@
+# The dictionary links contain the CSV files with all the data. The value for the key
+# GDP is the file that contains the GDP data. The value for the key unemployment contains the unemployment data.
+
+links={'GDP':'https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/PY0101EN/projects/coursera_project/clean_gdp.csv',
+       'unemployment':'https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/PY0101EN/projects/coursera_project/clean_unemployment.csv'}
